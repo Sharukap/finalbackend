ad922e709e19319469b565a98b587706aca7b36f
Sharuka Perera <sharukaperera98@gmail.com>
2020-08-15 10:59:18 +0530

    added views and sessions

--- backend/routes/adminRouter.js
+++ backend/routes/adminRouter.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const Students = require('../models/students');
-
 const adminRouter = express.Router();
-
 adminRouter.use(bodyParser.json());
 
 adminRouter.route('/')
+
 .get((req,res,next) => {
     Students.find({})
     .then((students) => {
@@ -27,6 +25,16 @@ adminRouter.route('/')
         res.json(student);
     }, (err) => next(err))
     .catch((err) => next(err));
+})
+.put((req,res,next) => {
+    res.statusCode = 403;
+    res.end("PUT Operation not supported on /students");
+
+})
+
+.delete((req, res, next) => {
+    res.statusCode = 403;
+    res.end("Not allowed to delete all records at once");
 });
 
 adminRouter.route('/:studentId')
@@ -42,6 +50,25 @@ adminRouter.route('/:studentId')
 .post((req, res, next) => {
     res.statusCode = 403;
     res.end('Cannot create a student record with'+ req.params.studentId+'Do not include an ID when creating a new record');
+})
+
+.put((req, res, next) => {
+    Students.findByIdAndUpdate(req.params.studentId,{
+        $set: req.body
+    },{new: true })
+    .then((student)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(student);
+    }, (err)=>next(err))
+    .catch((err)=> next(err));
+})
+
+.delete ((req, res, next) => {
+    Students.findByIdAndRemove(req.params.studentId, function (err, resp) { 
+	if (err) throw err;
+        res.json();
+    });
 });
 
 module.exports = adminRouter;
